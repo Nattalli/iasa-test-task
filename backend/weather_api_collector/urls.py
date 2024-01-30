5d73f05dfd01e745b0b7f52e9a56b9ddfb3e87fc
Nataliia <dominok56@gmail.com>
2024-01-30 21:26:13 +0200

    added possibility to take historical data by latitude and longitude

--- backend/weather_api_collector/urls.py
+++ backend/weather_api_collector/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CityListView, CountryAverageView
+from .views import CityListView, CountryAverageView, WeatherDataView
 
 urlpatterns = [
-    path('city/', CityListView.as_view(), name='city_list_api'),
-    path('country-average/<str:country>/', CountryAverageView.as_view(), name='country_average_api'),
+    path('city/', CityListView.as_view(), name='cities'),
+    path('country-average/<str:country>/', CountryAverageView.as_view(), name='country_average_data'),
+    path('weather-data/<str:lat>/<str:lng>/', WeatherDataView.as_view(), name='weather_historical_data'),
 ]
--- backend/weather_api_collector/views.py
+++ backend/weather_api_collector/views.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+import requests
+from dateutil.relativedelta import relativedelta
 from django.db.models import Avg
 from rest_framework import generics
 from rest_framework.response import Response
@@ -32,3 +36,25 @@ class CountryAverageView(generics.RetrieveAPIView):
 
         serializer = self.get_serializer(average_values)
         return Response(serializer.data)
+
+
+class WeatherDataView(generics.RetrieveAPIView):
+    def get(self, request, *args, **kwargs):
+        lat = self.kwargs.get('lat')
+        lng = self.kwargs.get('lng')
+
+        if not lat or not lng:
+            return Response({'error': 'Latitude and longitude parameters are required'}, status=400)
+
+        start_date = request.query_params.get('start_date', (datetime.now() - relativedelta(years=1)).strftime('%Y-%m-%d'))
+        end_date = request.query_params.get('end_date', datetime.now().strftime('%Y-%m-%d'))
+
+        api_url = f"https://archive-api.open-meteo.com/v1/era5?latitude={lat}&longitude={lng}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m"
+
+        try:
+            response = requests.get(api_url)
+            response.raise_for_status()
+            weather_data = response.json()
+            return Response(weather_data)
+        except requests.exceptions.RequestException as e:
+            return Response({'error': f'Request to open-meteo API failed: {str(e)}'}, status=500)
