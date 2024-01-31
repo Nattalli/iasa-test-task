c74f37b2bc7bf7f1c5c3cafd98dea08cfb17c054
Nataliia <dominok56@gmail.com>
2024-01-31 19:56:47 +0200

    improved accuracy of the forecasted weather

--- backend/weather_api_collector/views.py
+++ backend/weather_api_collector/views.py
@@ -48,24 +48,29 @@ class WeatherDataView(generics.RetrieveAPIView):
         if not lat or not lng:
             return Response({'error': 'Latitude and longitude parameters are required'}, status=400)
 
-        # Оригінальний запит на прогноз
         start_date = request.query_params.get('start_date',
                                               (datetime.now() - relativedelta(years=1)).strftime('%Y-%m-%d'))
         end_date = request.query_params.get('end_date', datetime.now().strftime('%Y-%m-%d'))
 
-        api_url = f"https://archive-api.open-meteo.com/v1/era5?latitude={lat}&longitude={lng}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m"
+        api_url = f"https://archive-api.open-meteo.com/v1/era5?latitude={lat}&longitude={lng}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
 
         try:
             response = requests.get(api_url)
             response.raise_for_status()
             weather_data = response.json()
 
-            data = {'time': weather_data['hourly']['time'], 'temperature_2m': weather_data['hourly']['temperature_2m']}
+            data = {
+                'time': weather_data['hourly']['time'],
+                'temperature_2m': weather_data['hourly']['temperature_2m'],
+                'relative_humidity_2m': weather_data['hourly']['relative_humidity_2m'],
+                'wind_speed_10m': weather_data['hourly']['wind_speed_10m']
+            }
+
             df = pd.DataFrame(data)
             df = df.dropna()
 
             df['time'] = pd.to_datetime(df['time'])
-            df.columns = ['ds', 'temperature_2m']
+            df.columns = ['ds', 'temperature_2m', 'relative_humidity_2m', 'wind_speed_10m']
 
             model = ExponentialSmoothing(df['temperature_2m'], seasonal='add', seasonal_periods=24)
             fit_model = model.fit()
@@ -81,29 +86,8 @@ class WeatherDataView(generics.RetrieveAPIView):
                 }
             }
 
-            key_indicators = df[['temperature_2m']].describe()
+            key_indicators = df[['temperature_2m', 'relative_humidity_2m', 'wind_speed_10m']].describe()
             response_data['key_indicators'] = key_indicators.to_dict()
-
-            additional_api_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lng}&past_days=10&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
-            additional_response = requests.get(additional_api_url)
-            additional_response.raise_for_status()
-            additional_weather_data = additional_response.json()
-
-            additional_data = {
-                'time': additional_weather_data['hourly']['time'],
-                'temperature_2m': additional_weather_data['hourly']['temperature_2m'],
-                'relative_humidity_2m': additional_weather_data['hourly']['relative_humidity_2m'],
-                'wind_speed_10m': additional_weather_data['hourly']['wind_speed_10m']
-            }
-
-            additional_df = pd.DataFrame(additional_data)
-            additional_df = additional_df.dropna()
-
-            additional_df['time'] = pd.to_datetime(additional_df['time'])
-            additional_df.columns = ['ds', 'temperature_2m', 'relative_humidity_2m', 'wind_speed_10m']
-
-            additional_key_indicators = additional_df[['temperature_2m', 'relative_humidity_2m', 'wind_speed_10m']].describe()
-            response_data['additional_key_indicators'] = additional_key_indicators.to_dict()
 
             sentence = generate_sentence(response_data['key_indicators'])
             response_data['sentence'] = sentence
@@ -115,17 +99,12 @@ class WeatherDataView(generics.RetrieveAPIView):
 
 
 def generate_sentence(key_indicators):
-    temperature_mean = key_indicators['temperature_2m']['mean']
-    temperature_range = key_indicators['temperature_2m']['max'] - key_indicators['temperature_2m']['min']
-    temperature_std = key_indicators['temperature_2m']['std']
+    humidity_mean = key_indicators['relative_humidity_2m']['mean']
 
-    if temperature_std < 5:
-        temperature_variation = "stably"
-    elif temperature_std < 10:
-        temperature_variation = "moderately"
-    else:
-        temperature_variation = "significantly"
+    wind_mean = key_indicators['wind_speed_10m']['mean']
+    wind_std = key_indicators['wind_speed_10m']['std']
 
-    sentence = f"The temperature has been averaging around {temperature_mean:.2f}°C with a {temperature_variation} {temperature_std:.2f}°C variation over time. The temperature range has been between {temperature_mean - temperature_range/2:.2f}°C and {temperature_mean + temperature_range/2:.2f}°C."
+    sentence = f"The humidity has been around {humidity_mean:.2f}%. " \
+               f"The wind speed has been averaging {wind_mean:.2f} m/s with a {wind_std:.2f} m/s variation."
 
     return sentence
